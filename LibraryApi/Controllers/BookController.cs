d68407218a000c9c87540440fe5a56d4abaa0eca
Efe <efeok.dev@gmail.com>
2026-04-25 12:42:49 +0300

    Author'a CRUD eklendi, jwt ekleniyor

--- LibraryApi/Controllers/BookController.cs
+++ LibraryApi/Controllers/BookController.cs
@@ -1,5 +1,6 @@
 ﻿using LibraryApi.Data;
 using LibraryApi.DTOs;
+using LibraryApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -61,6 +62,71 @@ namespace LibraryApi.Controllers
             };
 
             return Ok(dto);
+        }
+
+        [HttpPost("CreateBook")]
+        public ActionResult<BookCreateDto> Create([FromBody] BookCreateDto bookCreateDto) 
+        {
+            if (string.IsNullOrEmpty(bookCreateDto.Title))
+                return BadRequest(new { error = "Title is required" });
+            if (bookCreateDto.Year < 1000 || bookCreateDto.Year > DateTime.Now.Year || bookCreateDto.Year == null)
+                return BadRequest(new { error = "Year is not valid" });
+
+            var authorExists = _context.Authors.Any(a => a.Id == bookCreateDto.AuthorId);
+            if (!authorExists)
+                return BadRequest(new { error = $"Author with id {bookCreateDto.AuthorId} not found" });
+
+            var book = new Book
+            {
+                Title = bookCreateDto.Title,
+                AuthorId = bookCreateDto.AuthorId,
+                Year = bookCreateDto.Year,
+                Category = bookCreateDto.Category
+            };
+
+            _context.Books.Add(book);
+            _context.SaveChanges();
+
+            _context.Entry(book).Reference(b => b.Author).Load();
+
+            var result = new BookDto
+            {
+                Title = book.Title,
+                Year = book.Year,
+                AuthorName = book.Author?.FullName,
+                Category = book.Category
+            };
+            
+            return CreatedAtAction(nameof(GetById), new {id = book.Id},result); // 201 created
+        }
+        [HttpPut("Update/{id}")]
+        public ActionResult<BookUpdateDto> Update(int id, [FromBody] BookUpdateDto updateDto) 
+        {
+            var book = _context.Books.Find(id);
+            if (book == null)
+                return NotFound();
+            if (string.IsNullOrWhiteSpace(updateDto.Title))
+                return BadRequest(new { error = "Title is required" });
+
+            book.Title = updateDto.Title;
+            book.Year = updateDto.Year;
+            book.Category = updateDto.Category;
+            book.AuthorId = updateDto.AuthorId;
+            _context.SaveChanges();
+            return NoContent(); // 204
+        }
+
+        [HttpPut("Delete/{id}")]
+        public ActionResult<BookUpdateDto> Delete(int id)
+        {
+            var book = _context.Books.Find(id);
+            if (book == null)
+                return NotFound();
+
+
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+            return NoContent(); // 204
         }
     }
 }
