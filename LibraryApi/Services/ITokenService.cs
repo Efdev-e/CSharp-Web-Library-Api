fa0d387a8f4bd5d4aebf8bbd15d44677d42734a8
Efe <efeok.dev@gmail.com>
2026-04-25 13:01:38 +0300

    Commit

---
+++ LibraryApi/Services/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using LibraryApi.Models;
+
+namespace LibraryApi.Services
+{
+    public interface ITokenService
+    {
+        (string Token, DateTime ExpiresAt) GenerateToken(User user);
+    }
+}
---
+++ LibraryApi/Services/TokenService.cs
@@ -0,0 +1,14 @@
+﻿using LibraryApi.Models;
+
+namespace LibraryApi.Services
+{
+    public class TokenService : ITokenService
+    {
+        public TokenService() { }
+
+        public (string Token, DateTime ExpiresAt) GenerateToken(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
