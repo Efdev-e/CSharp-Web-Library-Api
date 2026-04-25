863240806162be5b2f76ada0eea2f8dd894b238a
Efe <efeok.dev@gmail.com>
2026-04-25 12:54:59 +0300

    JWT,Login Response Dto, Login Request Dto

---
+++ LibraryApi/DTOs/LoginResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryApi.DTOs
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; } = string.Empty;
+
+        public DateTime ExpiryDate { get; set; }
+
+        public string Username { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
+    }
+}
